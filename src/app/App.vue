4af7a1f08e69874a0ebcdd7597bff8b2a88e69e5
Pavel Shakhov <ab.pongo@gmail.com>
2025-07-27 22:32:02 +0300

    LoadingSpinner с задержкой

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts">
 import AppSidebar from "@/components/AppSidebar.vue";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import LoadingSpinner from "@/components/LoadingSpinner.vue";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+let loadingTimer: number;
+const isLoading = ref(false);
+
+router.beforeEach(() => {
+  loadingTimer = setTimeout(() => {
+    isLoading.value = true;
+  }, 200);
+});
+
+router.afterEach(() => {
+  clearTimeout(loadingTimer);
+  isLoading.value = false;
+});
 </script>
 
 <template>
@@ -11,7 +29,9 @@ import { SidebarProvider } from "@/components/ui/sidebar";
         <!-- <SidebarTrigger /> -->
         <div class="absolute left-0 flex min-h-full w-full justify-center">
           <div class="w-full max-w-4xl p-6">
-            <RouterView :key="$route.fullPath" />
+            <RouterView v-slot="{ Component }" :key="$route.fullPath">
+              <component :is="isLoading ? LoadingSpinner : Component"></component>
+            </RouterView>
           </div>
         </div>
       </main>
---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="h-8 w-8 animate-spin rounded-full border-4 border-blue-600 border-t-transparent" />
+</template>
