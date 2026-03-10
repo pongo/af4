e3456795effe45c004cc03f168c95fdc19a3d256
Pavel Shakhov <ab.pongo@gmail.com>
2026-03-10 22:27:45 +0300

    build: integrate vite-plugin-github-pages-spa for Github SPA routing support

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // import vueDevTools from "vite-plugin-vue-devtools";
-
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 import tailwindcss from "@tailwindcss/vite";
 import versionJson from "./version-json";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), vue(), versionJson()],
+  plugins: [tailwindcss(), vue(), versionJson(), githubPagesSpa()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
