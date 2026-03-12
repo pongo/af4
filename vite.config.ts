ad484dc69c3b09a19d8e81413a61dc7f0ffca8c0
Pavel Shakhov <ab.pongo@gmail.com>
2026-03-12 22:01:22 +0300

    build: replace external github-pages-spa plugin with local version

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // import vueDevTools from "vite-plugin-vue-devtools";
-import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 import tailwindcss from "@tailwindcss/vite";
 import versionJson from "./vite-plugins/version-json";
+import { githubPagesSpa } from "./vite-plugins/github-pages-spa";
 
 // https://vite.dev/config/
 export default defineConfig({
